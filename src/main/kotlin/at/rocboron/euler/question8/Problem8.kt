a403113e0f6f78de9a5c32f8846f99f2a5a93ab5
Roc Boronat <roc@fewlaps.com>
2019-04-04 21:22:59 -0400

    Just consider the number as a 1000 digits one instead of a 20x50 square of numbers, and use Long to avoid an Integer overflow

--- src/main/kotlin/at/rocboron/euler/question8/Problem8.kt
+++ src/main/kotlin/at/rocboron/euler/question8/Problem8.kt
@@ -1,7 +1,5 @@
 package at.rocboron.euler.question8
 
-import kotlin.math.max
-
 /** Largest product in a series
  *
  * The four adjacent digits in the 1000-digit number
@@ -33,90 +31,46 @@ import kotlin.math.max
  */
 class Problem8(private val adjacentNumbers: Int) {
 
-    val input = "" +
-            "73167176531330624919225119674426574742355349194934\n" +
-            "96983520312774506326239578318016984801869478851843\n" +
-            "85861560789112949495459501737958331952853208805511\n" +
-            "12540698747158523863050715693290963295227443043557\n" +
-            "66896648950445244523161731856403098711121722383113\n" +
-            "62229893423380308135336276614282806444486645238749\n" +
-            "30358907296290491560440772390713810515859307960866\n" +
-            "70172427121883998797908792274921901699720888093776\n" +
-            "65727333001053367881220235421809751254540594752243\n" +
-            "52584907711670556013604839586446706324415722155397\n" +
-            "53697817977846174064955149290862569321978468622482\n" +
-            "83972241375657056057490261407972968652414535100474\n" +
-            "82166370484403199890008895243450658541227588666881\n" +
-            "16427171479924442928230863465674813919123162824586\n" +
-            "17866458359124566529476545682848912883142607690042\n" +
-            "24219022671055626321111109370544217506941658960408\n" +
-            "07198403850962455444362981230987879927244284909188\n" +
-            "84580156166097919133875499200524063689912560717606\n" +
-            "05886116467109405077541002256983155200055935729725\n" +
+    val number = "" +
+            "73167176531330624919225119674426574742355349194934" +
+            "96983520312774506326239578318016984801869478851843" +
+            "85861560789112949495459501737958331952853208805511" +
+            "12540698747158523863050715693290963295227443043557" +
+            "66896648950445244523161731856403098711121722383113" +
+            "62229893423380308135336276614282806444486645238749" +
+            "30358907296290491560440772390713810515859307960866" +
+            "70172427121883998797908792274921901699720888093776" +
+            "65727333001053367881220235421809751254540594752243" +
+            "52584907711670556013604839586446706324415722155397" +
+            "53697817977846174064955149290862569321978468622482" +
+            "83972241375657056057490261407972968652414535100474" +
+            "82166370484403199890008895243450658541227588666881" +
+            "16427171479924442928230863465674813919123162824586" +
+            "17866458359124566529476545682848912883142607690042" +
+            "24219022671055626321111109370544217506941658960408" +
+            "07198403850962455444362981230987879927244284909188" +
+            "84580156166097919133875499200524063689912560717606" +
+            "05886116467109405077541002256983155200055935729725" +
             "71636269561882670428252483600823257530420752963450"
 
     fun run(): Long {
-        return findLargestProduct(input)
-    }
-
-    fun findLargestProduct(number: String): Long {
-        val horizontalStrips = number.split("\n")
-        val verticalStrips = getVerticalStrips(horizontalStrips)
-
-        println("Analyze horizontal strips")
-        val greatestHorizontalProduct = getGreatestProduct(horizontalStrips)
-        println("Analyze vertical strips")
-        val greatestVerticalProduct = getGreatestProduct(verticalStrips)
-
-        return max(greatestHorizontalProduct, greatestVerticalProduct)
-    }
-
-    fun getGreatestProduct(strips: List<String>): Long {
-        val stripLength = strips[0].length
         var greatestProduct = 0L
+        for (i in 0..1000 - adjacentNumbers) {
+            val numbersInString = number.substring(i, i + adjacentNumbers)
+            val numbersInChars = numbersInString.toCharArray().map { Integer.parseInt(it.toString()).toLong() }
 
-        for (stripIndex in 0 until strips.size) {
-            println("Analyzing row $stripIndex")
-            for (charIndex in 0..stripLength - adjacentNumbers) {
-                val numbersInString = strips[stripIndex].substring(charIndex, charIndex + adjacentNumbers)
-                val numbersInChars = numbersInString.toCharArray().map { Integer.parseInt(it.toString()).toLong() }
+            //println("\nCalculating numbersInChars")
+            val product = numbersInChars.reduce { acc, it ->
+                //println("acc = $acc, it = $it")
+                acc * it
+            }
+            //println("Result is $product")
 
-                val product = numbersInChars.reduce { acc, it ->
-                    //println("acc = $acc, it = $it")
-                    if (it != 0L) {
-                        acc * it
-                    } else {
-                        acc
-                    }
-                }
-
-                if (product < 0) {
-                    throw RuntimeException("This shouldn't happen")
-                }
-
-                if (product > greatestProduct) {
-                    greatestProduct = product
-                    println("New big! $numbersInChars product is $product")
-                } else {
-                    println("$numbersInChars product is too small")
-                }
+            if (product > greatestProduct) {
+                greatestProduct = product
+                println("New big! $numbersInChars product is $product")
             }
         }
         return greatestProduct
-    }
-
-    private fun getVerticalStrips(horizontalStrips: List<String>): List<String> {
-        val stripLength = horizontalStrips[0].length
-        val horizontalStripsCount = horizontalStrips.size
-
-        val verticalStrips = ArrayList<String>()
-        for (charIndex in 0 until stripLength) {
-            val sb = StringBuilder()
-            for (stripIndex in 0 until horizontalStripsCount) {
-                sb.append(horizontalStrips[stripIndex][charIndex])
-            }
-            verticalStrips.add(sb.toString())
-        }
-        return verticalStrips
     }
 }
--- src/test/kotlin/at/rocboron/euler/question8/Problem8Test.kt
+++ src/test/kotlin/at/rocboron/euler/question8/Problem8Test.kt
@@ -16,25 +16,4 @@ class Problem8Test {
         val value = Problem8(13).run()
         assertThat(value).isEqualTo(2091059712)
     }
-
-    @Test
-    fun canUseFirstNumbersInTheStrip() {
-        val list = listOf("9999999999999111")
-        val value = Problem8(13).getGreatestProduct(list)
-        assertThat(value).isEqualTo(2541865828329)
-    }
-
-    @Test
-    fun canUseLastNumbersInTheStrip() {
-        val list = listOf("1119999999999999")
-        val value = Problem8(13).getGreatestProduct(list)
-        assertThat(value).isEqualTo(2541865828329)
-    }
-
-    @Test
-    fun canReadVerticalStrips() {
-        val input = "6789\n6789\n6789"
-        val value = Problem8(3).findLargestProduct(input)
-        assertThat(value).isEqualTo(729)
-    }
 }
